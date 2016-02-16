80d4395e01d85347e28ea62e81c99c8568880665
ouzy <ousmane.mariko89@gmail.com>
2016-02-16 11:58:55 -0500

    no image page

--- page-templates/template-parts/content-single.php
+++ page-templates/template-parts/content-single.php
@@ -7,7 +7,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemType="http://schema.org/BlogPosting">
 	<?php tha_entry_top(); ?>
 	<?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ) ?>
-	<div class="hero-image" style="background-image:url(<?php echo $image_url[0] ?>)" title="<?php echo the_title_attribute( 'echo=0' ) ?>"></div>
+	<?php if($image_url != null) {?>
+		<div class="hero-image" style="background-image:url(<?php echo $image_url[0] ?>)" title="<?php echo the_title_attribute( 'echo=0' ) ?>"></div>
+	<?php } ?>
 	<div class="entry-content" itemprop="articleBody" >
 		<h2 class="entry-title" itemprop="name" ><?php the_title(); ?></h2>
 		<h3 class="sub-title"><?php the_field('subheader'); ?></h3>
