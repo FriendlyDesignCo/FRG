4815843f6eb911bc25d839228781afb07385d35f
ouzy <ousmane.mariko89@gmail.com>
2016-02-19 14:36:44 -0500

    beverages page

--- page-templates/beverages-page.php
+++ page-templates/beverages-page.php
@@ -11,6 +11,19 @@
 get_header(); ?>
 
 <div id="" class="beverages-page content-area">
+	<script>
+		(function($) {
+			
+		    $(document).ready(function() {
+			    
+			   	 $('#fullpage').fullpage({
+			   	 	verticalCentered: false,
+			   	 	sectionSelector: '.beverages-section'
+			   	 });
+			});
+			    
+		})(jQuery);
+	</script>
 	<style>
 		.arrow {
 			width: 100px;
@@ -28,60 +41,62 @@ get_header(); ?>
 			margin-bottom: -40px;
 		}
 	</style>
-	<section class="section beverages-section section-wrap slide-1">
-		<div class="section-inner ">
-			<h2 class="small"><?php the_field('hero_header'); ?></h2>
-			<?php the_field('hero_paragraph'); ?>
-			<p class="text-red medium"><?php the_field('scroll_more_text'); ?></p>
-			<div class="arrow-wrap">
-				<div class="arrow arrow-red"></div>
-				<div class="arrow arrow-red"></div>
-				<div class="arrow arrow-red"></div>
-			</div>
-		</div>
-	</section>
-	<?php if( have_rows('drinks') ): ?>
-	<?php $slide_number = 2; ?>
-	<?php while( have_rows('drinks') ): the_row(); ?>
-	<section class="section beverages-section beverage-slide section-wrap slide-"  style="background-image:url('<?php echo the_sub_field('background_image'); ?>')">
-		<div class="section-inner ">
-			<div class="beverage-image" style="background-image:url('<?php echo the_sub_field('drink_image'); ?>')">
-			</div>
-			<div class="beverage-info">
-				<h2 class=""><?php the_sub_field('header'); ?></h2>
-				<p><?php the_sub_field('paragraph_text'); ?></p>
-				<p class="intro-text"><?php the_sub_field('restaurant_intro_text'); ?></p>
-
-				<?php if( have_rows('drink_recipe') ): ?>
-				<div class="drink-block-wrap">
-				<?php while( have_rows('drink_recipe') ): the_row(); ?>
-					<div class="drink-block">
-						<h4 class="small"><?php the_sub_field('drink_name'); ?></h4>
-						<?php if( have_rows('ingrdients') ): ?>
-						<?php $ingredient_number = 1; ?>
-						<p>
-						<?php while( have_rows('ingrdients') ): the_row(); ?>
-						 
-						<?php if($ingredient_number > 1 ) { 
-							$ingrient_not_first = " | ".get_sub_field('ingredient');
-							echo $ingrient_not_first; 
-
-						} else {
-							the_sub_field('ingredient'); 
-						}?>
-						<?php ++$ingredient_number; ?>
-						<?php endwhile; ?>
-						</p>
-						<?php endif; ?>
-					</div>
-				<?php endwhile; ?>
+	<div id="fullpage">
+		<section class="section beverages-section section-wrap slide-1">
+			<div class="section-inner ">
+				<h2 class="small"><?php the_field('hero_header'); ?></h2>
+				<?php the_field('hero_paragraph'); ?>
+				<p class="text-red medium"><?php the_field('scroll_more_text'); ?></p>
+				<div class="arrow-wrap">
+					<div class="arrow arrow-red"></div>
+					<div class="arrow arrow-red"></div>
+					<div class="arrow arrow-red"></div>
 				</div>
-				<?php endif; ?>
 			</div>
-		</div>
-	</section>
-	<?php endwhile; ?>
-	<?php endif; ?>
+		</section>
+		<?php if( have_rows('drinks') ): ?>
+		<?php $slide_number = 2; ?>
+		<?php while( have_rows('drinks') ): the_row(); ?>
+		<section class="section beverages-section beverage-slide section-wrap slide-"  style="background-image:url('<?php echo the_sub_field('background_image'); ?>')">
+			<div class="section-inner ">
+				<div class="beverage-image" style="background-image:url('<?php echo the_sub_field('drink_image'); ?>')">
+				</div>
+				<div class="beverage-info">
+					<h2 class=""><?php the_sub_field('header'); ?></h2>
+					<p><?php the_sub_field('paragraph_text'); ?></p>
+					<p class="intro-text"><?php the_sub_field('restaurant_intro_text'); ?></p>
+
+					<?php if( have_rows('drink_recipe') ): ?>
+					<div class="drink-block-wrap">
+					<?php while( have_rows('drink_recipe') ): the_row(); ?>
+						<div class="drink-block">
+							<h4 class="small"><?php the_sub_field('drink_name'); ?></h4>
+							<?php if( have_rows('ingrdients') ): ?>
+							<?php $ingredient_number = 1; ?>
+							<p>
+							<?php while( have_rows('ingrdients') ): the_row(); ?>
+							 
+							<?php if($ingredient_number > 1 ) { 
+								$ingrient_not_first = " | ".get_sub_field('ingredient');
+								echo $ingrient_not_first; 
+
+							} else {
+								the_sub_field('ingredient'); 
+							}?>
+							<?php ++$ingredient_number; ?>
+							<?php endwhile; ?>
+							</p>
+							<?php endif; ?>
+						</div>
+					<?php endwhile; ?>
+					</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		</section>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
 </div>
 
 
