a4d06d91af2d57dd7033942db60af16e5cb41139
ouzy <ousmane.mariko89@gmail.com>
2016-02-22 12:44:44 -0500

    reservations page

---
+++ page-templates/reservations.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Reservations
+ *
+ * This template display content at full with, with no sidebars.
+ * Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site will use a different template.
+ *
+ * @package some_like_it_neat
+ */
+
+get_header(); ?>
+
+<div id="" class="cookbook-page content-area ">
+	
+	<div id="content" class="site-content">
+		<script type="text/javascript" src="https://secure.opentable.com/frontdoor/default.aspx?rid=24712&restref=24712&bgcolor=F6F6F3&titlecolor=0F0F0F&subtitlecolor=0F0F0F&btnbgimage=https://secure.opentable.com/frontdoor/img/ot_btn_red.png&otlink=FFFFFF&icon=dark&mode=short&hover=1"></script><a href="http://www.opentable.com/founding-farmers-dc-reservations-washington?rtype=ism&restref=24712" class="OT_ExtLink">Founding Farmers (24712), Washington, D.C. Area Reservations</a>
+	</div><!-- #content -->
+</div>
+
+
+<?php get_footer(); ?>
