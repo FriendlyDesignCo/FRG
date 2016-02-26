de9394c1ad3abae0f32955ef19d4e65bd26d0842
ouzy <ousmane.mariko89@gmail.com>
2016-02-26 10:00:40 -0500

    tweaks

--- header.php
+++ header.php
@@ -59,6 +59,16 @@
 	<link rel="stylesheet" id="" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" type="text/css" media="all">
 	<script src="http://www.wearefoundingfarmers.com/wp-content/themes/Founding-Farmers/assets/js/bootstrap-datetimepicker.min.js"></script>
 
+	<script>
+	 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	 ga('create', 'UA-41448103-1', 'auto');
+	 ga('send', 'pageview');
+
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
