1011b616a7f6c41cf9afe02cb6046f7813882ec7
ouzy <ousmane.mariko89@gmail.com>
2016-03-07 15:33:40 -0500

    add svg image upload support:

--- functions.php
+++ functions.php
@@ -399,3 +399,10 @@ function sidebar_text( $atts, $content = null ) {
     return '<div class="sidebar-text right">'.$sidebar_content.'</div>';
 }
 add_shortcode("sidebar_text", "sidebar_text");*/
+
+// add SVG images
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
